c0f63808bb5344b28a5e8e38dd67066cd5671a65
rayzathegrave <rdgraaf213@gmail.com>
2024-01-30 16:48:17 +0100

    begonnenv2

---
+++ src/main/java/nl/consumergram/consumergramv2/Consumergramv2Application.java
@@ -0,0 +1,13 @@
+package nl.consumergram.consumergramv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumergramv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumergramv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/consumergram/consumergramv2/Consumergramv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.consumergram.consumergramv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumergramv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
